abf4f5f722d6888c5f48c72adc4d432d3ee10172
Alan Macgowan <alanmacgowan@gmail.com>
2021-04-05 15:04:35 -0300

    Added Observer sample

---
+++ Observer/IBM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    class IBM : Stock
+    {
+        // Constructor
+
+        public IBM(string symbol, double price)
+          : base(symbol, price)
+        {
+        }
+    }
+}
---
+++ Observer/IInvestor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    interface IInvestor
+    {
+        void Update(Stock stock);
+    }
+}
