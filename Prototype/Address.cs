8b2fc00e0d3cb50b1776ff581d93f7f6c11731ea
Alan Macgowan <alanmacgowan@gmail.com>
2021-04-21 20:38:23 -0300

    Added Prototype sample

---
+++ Prototype/Address.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Address
+    {
+        public string address { get; set; }
+        public Address GetClone()
+        {
+            return (Address)this.MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Person
+    {
+        public int Age;
+        public DateTime BirthDate;
+        public string Name;
+        public Address Address;
+
+        public Person ShallowCopy()
+        {
+            return (Person)this.MemberwiseClone();
+        }
+
+        public Person DeepCopy()
+        {
+            Person clone = (Person)this.MemberwiseClone();
+            clone.Address = Address.GetClone();
+            return clone;
+        }
+    }
+}
