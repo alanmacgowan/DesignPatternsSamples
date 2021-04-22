968e6c7f2a634fb0dba35ec49a175e7bc42a4718
Alan Macgowan <alanmacgowan@gmail.com>
2021-04-22 00:23:17 -0300

    Added State sample

---
+++ State/ATMState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    public interface ATMState
+    {
+        void InsertDebitCard();
+        void EjectDebitCard();
+        void EnterPin();
+        void WithdrawMoney();
+    }
+}
---
+++ State/DebitCardInsertedState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    public class DebitCardInsertedState : ATMState
+    {
+        public void InsertDebitCard()
+        {
+            Console.WriteLine("You cannot insert the Debit Card, as the Debit card is already there ");
+        }
+        public void EjectDebitCard()
+        {
+            Console.WriteLine("Debit Card is ejected");
+        }
+        public void EnterPin()
+        {
+            Console.WriteLine("Pin number has been entered correctly");
+        }
+        public void WithdrawMoney()
+        {
+            Console.WriteLine("Money has been withdrawn");
+        }
+    }
+}
